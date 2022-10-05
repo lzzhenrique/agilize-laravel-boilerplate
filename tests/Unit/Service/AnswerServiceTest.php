5e50f57b452e2745f8db15d849aa61f95e193679
eng-luiz <luiz.silva@agilize.com.br>
2022-10-05 19:28:00 -0300

    adiciona testes ao service de answer

--- tests/Unit/Service/AnswerServiceTest.php
+++ tests/Unit/Service/AnswerServiceTest.php
@@ -90,5 +90,57 @@ class AnswerServiceTest extends TestCase
         );
     }
 
+    public function testCreateFunctionShouldReturnAExceptionIfAnswerDoesNotHaveAExistentQuestion()
+    {
+        $this->expectException(\Exception::class);
 
+        // given
+        $questionRepositoryMock = $this->createMock(QuestionRepository::class);
+        $questionRepositoryMock->method('getById')->willReturn(null);
+
+        $answerRepositoryMock = $this->createMock(AnswerRepository::class);
+
+        $answerService = new AnswerService($answerRepositoryMock, $questionRepositoryMock);
+
+        // when
+        $result = $answerService->create(
+            'neymar jr',
+            '001aeb1d-2569-a56d-287e-2afbbc9637a6',
+            true
+        );
+    }
+
+    public function isCorrectProvider()
+    {
+        return [
+            'inteiro' => [1],
+            'string preenchida' => ['true'],
+            'objeto' =>[new \DateTime()],
+        ];
+    }
+
+    /**
+     * @dataProvider  isCorrectProvider
+     */
+    public function testCreateFunctionShouldReturnAExceptionIfIscorrectHaveAValueDifferentThanABoolean($isCorrect)
+    {
+        $this->expectException(\Exception::class);
+
+        // given
+        $questionMock = $this->createMock(Question::class);
+
+        $questionRepositoryMock = $this->createMock(QuestionRepository::class);
+        $questionRepositoryMock->method('getById')->willReturn($questionMock);
+
+        $answerRepositoryMock = $this->createMock(AnswerRepository::class);
+
+        $answerService = new AnswerService($answerRepositoryMock, $questionRepositoryMock);
+
+        // when
+        $answerService->create(
+            'neymar jr',
+            '001aeb1d-2569-a56d-287e-2afbbc9637a6',
+            $isCorrect
+        );
+    }
 }
